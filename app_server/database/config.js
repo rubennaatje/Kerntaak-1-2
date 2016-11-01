021bc167665f5ffe306d6090392058e8201c5560
DaMaus <maurits_versluis@hotmail.com>
2016-11-01 14:36:55 +0100

    completed project

--- app_server/database/config.js
+++ app_server/database/config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    "database": "conference",
-    "user": "root",
-    "password": "",
+    "host": "37.60.237.115",
+    "database": "mauritsv_conference",
+    "user": "mauritsv_maus",
+    "password": "thispasswordisswag!",
     "dbport": 3306,
     "port":  process.env.PORT || 3000
 };
