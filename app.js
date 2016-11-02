351d40dddcea47758047db5ef06f39274b224f6c
DaMaus <maurits_versluis@hotmail.com>
2016-11-02 09:24:30 +0100

    completed project

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'twig');
 app.set('views', path.join(__dirname, 'app_server/views'));
 
 //// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
